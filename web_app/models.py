023a6d7eee8f4aa5d386d952ea4b9273a9f3e535
Mike Rossetti <s2t2@users.noreply.github.com>
2020-03-23 14:14:30 -0400

    Integrate with the db

--- web_app/models.py
+++ web_app/models.py
@@ -12,3 +12,23 @@ class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(128))
     author_id = db.Column(db.String(128))
+
+def parse_records(database_records):
+    """
+    Parses database records into a clean json-like structure
+    Param: database_records (a list of db.Model instances)
+    Example: parse_records(User.query.all())
+    Returns: a list of dictionaries, each corresponding to a record, like...
+        [
+            {"id": 1, "title": "Book 1"},
+            {"id": 2, "title": "Book 2"},
+            {"id": 3, "title": "Book 3"},
+        ]
+    """
+    parsed_records = []
+    for record in database_records:
+        print(record)
+        parsed_record = record.__dict__
+        del parsed_record["_sa_instance_state"]
+        parsed_records.append(parsed_record)
+    return parsed_records
--- web_app/routes/book_routes.py
+++ web_app/routes/book_routes.py
@@ -1,29 +1,34 @@
 
 # web_app/routes/book_routes.py
 
-from flask import Blueprint, jsonify, render_template, request
+from flask import Blueprint, jsonify, render_template, request, redirect
+
+from web_app.models import db, Book, parse_records
 
 book_routes = Blueprint("book_routes", __name__)
 
 @book_routes.route("/books.json")
 def list_books():
     print("REQUESTED THE BOOKS IN JSON FORMAT")
-    books = [
-        {"id": 1, "title": "Book 1"},
-        {"id": 2, "title": "Book 2"},
-        {"id": 3, "title": "Book 3"},
-    ] # todo: get from the database
+    #books = [
+    #    {"id": 1, "title": "Book 1"},
+    #    {"id": 2, "title": "Book 2"},
+    #    {"id": 3, "title": "Book 3"},
+    #] # todo: get from the database
+    book_records = Book.query.all()
+    books = parse_records(book_records)
     return jsonify(books)
 
 @book_routes.route("/books")
 def books():
     print("VISITED THE BOOKS PAGE")
-    books = [
-        {"id": 1, "title": "Book 1"},
-        {"id": 2, "title": "Book 2"},
-        {"id": 3, "title": "Book 3"},
-    ] # todo: get from the database
-    return render_template("books.html", books=books)
+    #books = [
+    #    {"id": 1, "title": "Book 1"},
+    #    {"id": 2, "title": "Book 2"},
+    #    {"id": 3, "title": "Book 3"},
+    #] # todo: get from the database
+    book_records = Book.query.all()
+    return render_template("books.html", books=book_records)
 
 @book_routes.route("/books/new")
 def new_book():
@@ -33,9 +38,13 @@ def new_book():
 def create_book():
     print("FORM DATA:", dict(request.form))
     # todo: store in database
-    return jsonify({
-        "message": "BOOK CREATED OK (TODO)",
-        "book": dict(request.form)
-    })
+    #return jsonify({
+    #    "message": "BOOK CREATED OK (TODO)",
+    #    "book": dict(request.form)
+    #})
+    new_book = Book(title=request.form["book_title"], author_id=request.form["author_name"])
+    print(new_book)
+    db.session.add(new_book)
+    db.session.commit()
     #flash(f"Book '{new_book.title}' created successfully!", "success")
-    #return redirect(f"/books")
+    return redirect("/books")
