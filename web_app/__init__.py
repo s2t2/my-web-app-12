77ac3db2eefef945939bb1c2a501f0ffa9ba05c8
Mike Rossetti <s2t2@users.noreply.github.com>
2020-03-23 14:00:57 -0400

    Generated development db

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -3,11 +3,18 @@
 
 from flask import Flask
 
+from web_app.models import db, migrate
 from web_app.routes.home_routes import home_routes
 from web_app.routes.book_routes import book_routes
 
 def create_app():
     app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///web_app_12.db"
+    #app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////Users/mjr/Desktop/web-app-inclass-11/web_app_12.db"
+    db.init_app(app)
+    migrate.init_app(app, db)
+
     app.register_blueprint(home_routes)
     app.register_blueprint(book_routes)
 
