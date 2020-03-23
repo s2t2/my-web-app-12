165d90a94bb54093384ab9b0e4ee7a39aff37337
Mike Rossetti <s2t2@users.noreply.github.com>
2020-03-23 13:11:05 -0400

    Render books page HTML template

--- web_app/routes/book_routes.py
+++ web_app/routes/book_routes.py
@@ -1,11 +1,21 @@
 
 # web_app/routes/book_routes.py
 
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, render_template
 
 book_routes = Blueprint("book_routes", __name__)
 
 @book_routes.route("/books.json")
+def list_books():
+    print("REQUESTED THE BOOKS IN JSON FORMAT")
+    books = [
+        {"id": 1, "title": "Book 1"},
+        {"id": 2, "title": "Book 2"},
+        {"id": 3, "title": "Book 3"},
+    ] # todo: get from the database
+    return jsonify(books)
+
+@book_routes.route("/books")
 def books():
     print("VISITED THE BOOKS PAGE")
     books = [
@@ -13,4 +23,4 @@ def books():
         {"id": 2, "title": "Book 2"},
         {"id": 3, "title": "Book 3"},
     ] # todo: get from the database
-    return jsonify(books)
+    return render_template("books.html")
