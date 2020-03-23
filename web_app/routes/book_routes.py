caea1bfdc3e4962b7ef3adbbd285e5c8fedf37c3
Mike Rossetti <s2t2@users.noreply.github.com>
2020-03-23 13:32:35 -0400

    Routing and views for creating new books

--- web_app/routes/book_routes.py
+++ web_app/routes/book_routes.py
@@ -1,7 +1,7 @@
 
 # web_app/routes/book_routes.py
 
-from flask import Blueprint, jsonify, render_template
+from flask import Blueprint, jsonify, render_template, request
 
 book_routes = Blueprint("book_routes", __name__)
 
@@ -24,3 +24,18 @@ def books():
         {"id": 3, "title": "Book 3"},
     ] # todo: get from the database
     return render_template("books.html", books=books)
+
+@book_routes.route("/books/new")
+def new_book():
+    return render_template("new_book.html")
+
+@book_routes.route("/books/create", methods=["POST"])
+def create_book():
+    print("FORM DATA:", dict(request.form))
+    # todo: store in database
+    return jsonify({
+        "message": "BOOK CREATED OK (TODO)",
+        "book": dict(request.form)
+    })
+    #flash(f"Book '{new_book.title}' created successfully!", "success")
+    #return redirect(f"/books")
