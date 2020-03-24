971f6a1586a2c563ade92d8a07ad79c299f719e7
Mike Rossetti <s2t2@users.noreply.github.com>
2020-03-24 14:22:46 -0400

    Store embeddings in the db as well

--- web_app/routes/twitter_routes.py
+++ web_app/routes/twitter_routes.py
@@ -29,9 +29,10 @@ def get_user(screen_name=None):
     #breakpoint()
 
     print("STATUS COUNT:", len(statuses))
-    #all_tweet_texts = [status.full_text for status in statuses]
-    #embeddings = list(basilica_api_client.embed_sentences(all_tweet_texts, model="twitter"))
-    #print("NUMBER OF EMBEDDINGS", len(embeddings))
+    basilica_api = basilica_api_client()
+    all_tweet_texts = [status.full_text for status in statuses]
+    embeddings = list(basilica_api.embed_sentences(all_tweet_texts, model="twitter"))
+    print("NUMBER OF EMBEDDINGS", len(embeddings))
 
     # TODO: explore using the zip() function maybe...
     counter = 0
@@ -45,12 +46,15 @@ def get_user(screen_name=None):
         db_tweet.user_id = status.author.id # or db_user.id
         db_tweet.full_text = status.full_text
         #embedding = basilica_client.embed_sentence(status.full_text, model="twitter") # todo: prefer to make a single request to basilica with all the tweet texts, instead of a request per tweet
-        #embedding = embeddings[counter]
-        #print(len(embedding))
-        #db_tweet.embedding = embedding
+        embedding = embeddings[counter]
+        print(len(embedding))
+        db_tweet.embedding = embedding
         db.session.add(db_tweet)
         counter+=1
     db.session.commit()
+
+
+    #breakpoint()
     return "OK"
 
     #return render_template("user.html", user=db_user, tweets=statuses) # tweets=db_tweets
