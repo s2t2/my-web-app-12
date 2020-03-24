fa400922f6eff8ad0c1fa608d339c1e0999f6ec4
Mike Rossetti <s2t2@users.noreply.github.com>
2020-03-24 13:44:27 -0400

    Continue working with twitter api to get user and tweet data

--- web_app/services/twitter_service.py
+++ web_app/services/twitter_service.py
@@ -4,6 +4,7 @@
 import os
 from dotenv import load_dotenv
 import tweepy
+from pprint import pprint
 
 load_dotenv()
 
@@ -14,9 +15,36 @@ access_token_secret = os.getenv("TWITTER_ACCESS_TOKEN_SECRET", default="OOPS")
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
+print("AUTH", type(auth))
 
 api = tweepy.API(auth)
+print("API", type(api)) #> <class 'tweepy.api.API'>
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+#breakpoint()
+#public_tweets = api.home_timeline()
+#for tweet in public_tweets:
+#    print(tweet.text)
+
+# get information about a twitter user:
+
+screen_name = "elonmusk"
+
+print("--------------")
+print("USER...")
+user = api.get_user(screen_name)
+print(type(user)) #> <class 'tweepy.models.User'>
+print(user.screen_name)
+print(user.followers_count)
+pprint(user._json)
+
+print("--------------")
+print("STATUSES...")
+# get that user's tweets:
+# see: http://docs.tweepy.org/en/latest/api.html#API.user_timeline
+statuses = api.user_timeline(screen_name, tweet_mode="extended", count=150, exclude_replies=True, include_rts=False)
+
+for status in statuses:
+    print(type(status)) #> <class 'tweepy.models.Status'>
+    #pprint(status._json)
+    #breakpoint()
+    print(status.full_text)
