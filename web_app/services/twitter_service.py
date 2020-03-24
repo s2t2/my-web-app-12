8c8bcdb64e765bb170dc14c8a0fe49486ecb07a7
Mike Rossetti <s2t2@users.noreply.github.com>
2020-03-24 13:17:50 -0400

    Beginnings of twitter service

---
+++ web_app/services/twitter_service.py
@@ -0,0 +1,22 @@
+
+# web_app/services/twitter_service.py
+
+import os
+from dotenv import load_dotenv
+import tweepy
+
+load_dotenv()
+
+consumer_key = os.getenv("TWITTER_API_KEY", default="OOPS")
+consumer_secret = os.getenv("TWITTER_API_SECRET", default="OOPS")
+access_token = os.getenv("TWITTER_ACCESS_TOKEN", default="OOPS")
+access_token_secret = os.getenv("TWITTER_ACCESS_TOKEN_SECRET", default="OOPS")
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)
