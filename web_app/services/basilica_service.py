d5cb03479cabcfee625467a459cca9d025025aef
Mike Rossetti <s2t2@users.noreply.github.com>
2020-03-24 12:57:38 -0400

    Add basilica service

---
+++ web_app/services/basilica_service.py
@@ -0,0 +1,27 @@
+# web_app/services/basilica_service.py
+
+import basilica
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("BASILICA_API_KEY")
+
+print("---------")
+connection = basilica.Connection(API_KEY)
+print(type(connection)) #> <class 'basilica.Connection'>
+
+print("---------")
+sentence = "Hello again"
+sent_embeddings = connection.embed_sentence(sentence)
+print(list(sent_embeddings))
+
+print("---------")
+sentences = ["Hello world!", "How are you?"]
+print(sentences)
+
+embeddings = connection.embed_sentences(sentences)
+print("EMBEDDINGS...")
+print(type(embeddings))
+print(list(embeddings)) # [[0.8556405305862427, ...], ...]
